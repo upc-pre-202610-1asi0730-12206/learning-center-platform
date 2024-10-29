0faf8ee563f25dd451f611f4264201a2864f356a
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-29 16:07:19 -0500

    chore: add dependency injection mapping rules.

--- ACME.LearningCenterPlatform.API/Program.cs
+++ ACME.LearningCenterPlatform.API/Program.cs
@@ -1,3 +1,5 @@
+using ACME.LearningCenterPlatform.API.Publishing.Application.Internal.CommandServices;
+using ACME.LearningCenterPlatform.API.Publishing.Application.Internal.QueryServices;
 using ACME.LearningCenterPlatform.API.Publishing.Domain.Repositories;
 using ACME.LearningCenterPlatform.API.Publishing.Domain.Services;
 using ACME.LearningCenterPlatform.API.Publishing.Infrastructure.Persistence.EFC.Repositories;
@@ -50,7 +52,10 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 // Publishing Bounded Context
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ITutorialRepository, TutorialRepository>();
-
+builder.Services.AddScoped<ICategoryCommandService, CategoryCommandService>();
+builder.Services.AddScoped<ICategoryQueryService, CategoryQueryService>();
+builder.Services.AddScoped<ITutorialCommandService, TutorialCommandService>();
+builder.Services.AddScoped<ITutorialQueryService, TutorialQueryService>();
 
 var app = builder.Build();
 
