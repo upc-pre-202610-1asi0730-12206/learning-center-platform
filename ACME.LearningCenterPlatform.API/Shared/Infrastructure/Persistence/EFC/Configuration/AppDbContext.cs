7970f1dec7b7966fc9d7103ba3a43906a05b49e6
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-16 21:02:30 -0500

    refactor: relocate publishing and profiles bounded context orm mapping rules into bounded context model builder extensions.

--- ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,7 +1,6 @@
 using ACME.LearningCenterPlatform.API.IAM.Domain.Model.Aggregates;
-using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Aggregates;
-using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregate;
-using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+using ACME.LearningCenterPlatform.API.Profiles.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using ACME.LearningCenterPlatform.API.Publishing.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -46,62 +45,10 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
         base.OnModelCreating(builder);
 
         // Publishing Context
-        builder.Entity<Category>().HasKey(c => c.Id);
-        builder.Entity<Category>().Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<Category>().Property(c => c.Name).IsRequired().HasMaxLength(30);
-
-        builder.Entity<Tutorial>().HasKey(t => t.Id);
-        builder.Entity<Tutorial>().Property(t => t.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<Tutorial>().Property(t => t.Title).IsRequired().HasMaxLength(50);
-        builder.Entity<Tutorial>().Property(t => t.Summary).IsRequired().HasMaxLength(240);
-
-        builder.Entity<Asset>().HasDiscriminator(a => a.Type);
-        builder.Entity<Asset>().HasKey(a => a.Id);
-        builder.Entity<Asset>().HasDiscriminator<string>("asset_type")
-            .HasValue<Asset>("asset_base")
-            .HasValue<ImageAsset>("asset_image")
-            .HasValue<VideoAsset>("asset_video")
-            .HasValue<ReadableContentAsset>("asset_readable_content");
-
-        builder.Entity<Asset>().OwnsOne(i => i.AssetIdentifier, ai =>
-        {
-            ai.WithOwner().HasForeignKey("Id");
-            ai.Property(p => p.Identifier).HasColumnName("AssetIdentifier");
-        });
-        builder.Entity<ImageAsset>().Property(p => p.ImageUri).IsRequired();
-        builder.Entity<VideoAsset>().Property(p => p.VideoUri).IsRequired();
-
-        builder.Entity<Tutorial>().HasMany(t => t.Assets);
+        builder.ApplyPublishingConfiguration();
 
         // Profiles Context
-        
-        builder.Entity<Profile>().HasKey(p => p.Id);
-        builder.Entity<Profile>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<Profile>().OwnsOne(p => p.Name,
-            n =>
-            {
-                n.WithOwner().HasForeignKey("Id");
-                n.Property(p => p.FirstName).HasColumnName("FirstName");
-                n.Property(p => p.LastName).HasColumnName("LastName");
-            });
-
-        builder.Entity<Profile>().OwnsOne(p => p.Email,
-            e =>
-            {
-                e.WithOwner().HasForeignKey("Id");
-                e.Property(a => a.Address).HasColumnName("EmailAddress");
-            });
-
-        builder.Entity<Profile>().OwnsOne(p => p.Address,
-            a =>
-            {
-                a.WithOwner().HasForeignKey("Id");
-                a.Property(s => s.Street).HasColumnName("AddressStreet");
-                a.Property(s => s.Number).HasColumnName("AddressNumber");
-                a.Property(s => s.City).HasColumnName("AddressCity");
-                a.Property(s => s.PostalCode).HasColumnName("AddressPostalCode");
-                a.Property(s => s.Country).HasColumnName("AddressCountry");
-            });
+        builder.ApplyProfilesConfiguration();
         
         // IAM Context
         
