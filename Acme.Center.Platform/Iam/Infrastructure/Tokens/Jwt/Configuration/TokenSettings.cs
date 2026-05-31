b3eed94b539849721446cd3c0186e7dc0cde75da
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-31 07:48:54 -0500

    refactor(iam): update token-settings secret property to use required modifier.

--- Acme.Center.Platform/Iam/Infrastructure/Tokens/Jwt/Configuration/TokenSettings.cs
+++ Acme.Center.Platform/Iam/Infrastructure/Tokens/Jwt/Configuration/TokenSettings.cs
@@ -8,5 +8,5 @@ namespace Acme.Center.Platform.Iam.Infrastructure.Tokens.Jwt.Configuration;
  */
 public class TokenSettings
 {
-    public string Secret { get; set; }
+    public required string Secret { get; set; }
 }
